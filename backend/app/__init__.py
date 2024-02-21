5bd813706964817b0d663b6cf6d5d06cdbb12e87
sanketwin <sanketwin@gmail.com>
2024-02-21 20:15:00 +0530

    cors handled

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from flask_cors import CORS
 from .extensions import api, db, jwt
 from .users import user
 from .properties import ns
 
 
+
 def create_app():
     app = Flask(__name__)
+
+    CORS(app)
 
     app.config["SQLALCHEMY_DATABASE_URI"]="mysql://root:root@localhost/lease"       #mysql://username:password@localhost/database
     app.config["JWT_SECRET_KEY"] = "f3422f49517f852da686694bc2f01fac"
